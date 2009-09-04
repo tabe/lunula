361ffbff82d58e78f0575477fb6341772009354f
Takeshi Abe <tabe@fixedpoint.jp>
2009-09-04 18:06:19 +0900

    (lunula test) superceded by (xunit)

--- tests/lunula/uri.scm
+++ tests/lunula/uri.scm
@@ -1,9 +1,7 @@
 #!/usr/bin/env ypsilon
 #!r6rs
 
-(import (rnrs)
-        (lunula uri)
-        (lunula test))
+(import (lunula uri) (xunit))
 
 (assert-string=? "/path/example" (url->path "/path/example#abc"))
 (assert-string=? "abc" (url->fragment "/path/example#abc"))

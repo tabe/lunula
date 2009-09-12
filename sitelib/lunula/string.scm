d58672c72c6462079c8428c0933d5e5a8908f7ad
Takeshi Abe <tabe@fixedpoint.jp>
2009-09-12 09:48:02 +0900

    added blank?

--- sitelib/lunula/string.scm
+++ sitelib/lunula/string.scm
@@ -1,7 +1,11 @@
 (library (lunula string)
-  (export string-underscore string-hyphen)
+  (export blank? string-underscore string-hyphen)
   (import (rnrs)
-          (only (srfi :13) string-map))
+          (only (srfi :13) string-map string-null?))
+
+  (define (blank? x)
+    (or (not x)
+        (and (string? x) (string-null? x))))
 
   (define (string-underscore str)
     (string-map (lambda (c) (if (char=? #\- c) #\_ c)) str))
--- tests/lunula/string.scm
+++ tests/lunula/string.scm
@@ -1,4 +1,15 @@
 #!/usr/bin/env ypsilon
 #!r6rs
 
-(import (lunula string))
+(import (lunula string) (xunit))
+
+(assert-string=? "call_with_current_continuation" (string-underscore "call-with-current-continuation"))
+
+(assert-string=? "call-with-current-continuation" (string-hyphen "call_with_current_continuation"))
+
+(assert-boolean=? #t (blank? #f))
+(assert-boolean=? #t (blank? ""))
+(assert-boolean=? #f (blank? 0))
+(assert-boolean=? #f (blank? "0"))
+
+(report)

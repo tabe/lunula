944e62794805d8c935a4822383dd728d2f956051
Takeshi Abe <tabe@fixedpoint.jp>
2009-09-15 13:24:37 +0900

    environment variable LUNULA_LOG_DRAIN

--- sitelib/lunula/log.scm
+++ sitelib/lunula/log.scm
@@ -2,12 +2,18 @@
   (export drain
           info)
   (import (rnrs)
-          (only (core) make-parameter format))
+          (only (core) make-parameter format lookup-process-environment))
 
-  (define drain (make-parameter #t))
+  (define drain
+    (make-parameter
+     (cond ((lookup-process-environment "LUNULA_LOG_DRAIN")
+            => (lambda (path) (open-file-output-port path (file-options no-fail))))
+           (else #t))))
 
   (define (info msg . args)
     (assert (string? msg))
-    (apply format (drain) (string-append msg "~&") args))
+    (let ((port (drain)))
+      (apply format port (string-append msg "~%") args)
+      (when (output-port? port) (flush-output-port port))))
 
 )

175931c492daee3171b7ded691296f632d2b3889
Takeshi Abe <tabe@fixedpoint.jp>
2010-01-30 23:31:02 +0900

    build input field dynamically with procedures

--- tests/lunula/input-field.scm
+++ tests/lunula/input-field.scm
@@ -5,16 +5,27 @@
         (xunit))
 
 (define-record-type record-to-test-add-input-fields
-  (fields x y z))
+  (fields x y z z1))
 
 (define select-foo '(select (a "a") (b "b") (c "c")))
+
+(define radio-bar `(procedure ,(lambda () '(radio (a "A" #t) (b "B" #f) (c "C" #f)))))
 
 (add-input-fields record-to-test-add-input-fields
   (#f
    (text "this is y.")
-   select-foo))
+   select-foo
+   radio-bar))
 
-(assert-equal? `(#f text ,select-foo) (input-types (record-type-descriptor record-to-test-add-input-fields)))
-(assert-equal? '(#f "this is y." #f) (input-descriptions (record-type-descriptor record-to-test-add-input-fields)))
+(assert-equal? `(#f text ,select-foo ,radio-bar) (input-types (record-type-descriptor record-to-test-add-input-fields)))
+(assert-equal? '(#f "this is y." #f #f) (input-descriptions (record-type-descriptor record-to-test-add-input-fields)))
+
+(assert-equal? '(#\< input (#\space type "='" text "'") (#\space name "='" "y" "'") (#\space value "='" "..." "'") " />")
+               (input-field 'text "y" "..."))
+(assert-equal? '(#\< select (#\space name "='" "z" "'") #\>
+                 ((#\< option (#\space value "='" a "'") () #\> "a" "</" option "\n>") (#\< option (#\space value "='" b "'") (#\space selected) #\> "b" "</" option "\n>") (#\< option (#\space value "='" c "'") () #\> "c" "</" option "\n>"))
+                 "</" select "\n>")
+               (input-field select-foo "z" "b"))
+(assert-list? (input-field radio-bar "z1" "b"))
 
 (report)
